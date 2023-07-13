904065487c2f19938588ad5d3b448ff9a5e723cc
Sebastian Montero <sebastian.montero@gmail.com>
2023-07-13 17:04:02 -0400

    Updated the wallet and Bitcoin util interface.

--- src/models/interfaces.ts
+++ src/models/interfaces.ts
@@ -1,3 +1,19 @@
+import { Trx } from "./types"
+
+
+type BalancePayload = {
+  confirmed: number
+  immature: number
+  trusted_pending: number
+  untrusted_pending: number
+}
+
+type FinalizeTrxResponse = {
+  trx_id: string
+  trx: any
+  psbt: string
+}
+
 export interface Wallet {
 
   /**
@@ -14,11 +30,13 @@ export interface Wallet {
   * @param {string} fee_rate
   * @returns {Promise<any>}
   */
-  build_trx(address: string, amount: string, fee_rate: string): Promise<any>;
+  build_trx(address: string, amount: string, fee_rate: string): Promise<Trx>;
   /**
   * @returns {any}
   */
-  get_balance(): any;
+  get_balance(): BalancePayload;
+
+  finalize(psbts: string[], broadcast: boolean): Promise<FinalizeTrxResponse>;
 }
 
 
@@ -26,5 +44,6 @@ export interface BitcoinUtil {
   walletSyncTimeGap: number //minutes that have to pass after the last sync, to require another sync when performing an operation
   toDescriptor(miniscript: string): string
   createWallet(descriptor): Wallet
+  canFinalizePsbt(psbts: string[]): boolean
 }
 
