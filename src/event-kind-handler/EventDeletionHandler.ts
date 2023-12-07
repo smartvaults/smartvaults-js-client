b39a1fcb8e0bb2ca41eb86fb44ab8d4057c5b3a0
Dreyhh <d.reyhh13@gmail.com>
2023-12-07 11:54:20 -0600

    Changes EventDeletionHanlder to account for deletion of direct messages

--- src/event-kind-handler/EventDeletionHandler.ts
+++ src/event-kind-handler/EventDeletionHandler.ts
@@ -1,13 +1,16 @@
-import { type Event } from 'nostr-tools'
+import { Kind, type Event } from 'nostr-tools'
 import { TagType, StoreKind } from '../enum'
 import { type Store } from '../service'
 import { getTagValues } from '../util'
 import { EventKindHandler } from './EventKindHandler'
+import { Chat } from '../models/Chat'
 export class EventDeletionHandler extends EventKindHandler {
   private readonly stores: Map<number, Store>
-  constructor(stores: Map<number, Store>) {
+  private readonly getChat: () => Chat
+  constructor(stores: Map<number, Store>, getChat: () => Chat) {
     super()
     this.stores = stores
+    this.getChat = getChat
   }
 
 
@@ -18,20 +21,31 @@ export class EventDeletionHandler extends EventKindHandler {
     for (const deletionEvent of deletionEvents) {
       const ids: string[] = getTagValues(deletionEvent, TagType.Event);
       const rawEvents: Array<Event<K>> = eventsStore.getManyAsArray(ids)
-      rawEvents.forEach((event) => {
+      for (const event of rawEvents) {
         if (deletionEvent.pubkey === event.pubkey) {
           const { kind, id } = event;
           const storeKind = this.stores.get(kind)!;
           const storedValue = storeKind.get(id);
           if (storeKind) {
             storeKind.delete(storedValue);
+            if (kind === Kind.EncryptedDirectMessage) {
+              try {
+                const conversation = await this.getChat().getConversation(storedValue.conversationId)
+                conversation.messages.remove(storedValue.id)
+                const messageDeleted = { ...storedValue, message: "This message has been deleted" }
+                conversation.messages.insertSorted(messageDeleted)
+                conversation.hasUnreadMessages = true
+              } catch (e) {
+                console.log(e)
+              }
+            }
             rawEventsToDelete.push(event);
             const existingPayload = payloadMap.get(kind) || [];
             existingPayload.push(id);
             payloadMap.set(kind, existingPayload);
           }
         }
-      });
+      };
     }
     eventsStore.delete(rawEventsToDelete)
     return [payloadMap]
